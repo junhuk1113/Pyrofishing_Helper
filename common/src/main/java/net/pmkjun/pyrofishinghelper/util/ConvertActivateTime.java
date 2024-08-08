55ef8aa6ec8bb579a160705bfec800b2db9586bb
junhuk1113 <junhuk1113@naver.com>
2024-08-08 10:29:03 +0000

    토템 지속시간/쿨타임 변경, 물고기 카운트 안되는 오류 수정

--- common/src/main/java/net/pmkjun/pyrofishinghelper/util/ConvertActivateTime.java
+++ common/src/main/java/net/pmkjun/pyrofishinghelper/util/ConvertActivateTime.java
@@ -5,11 +5,11 @@ public class ConvertActivateTime {
 
     }
     public static int asMinute(int level){
-        if(level<=15) return 5+level;
-        else return 20 + (2 * (level-15));
+        if(level<=20) return 5+level;
+        else return 25 + (2 * (level-20));
     }
     public static int asLevel(int minute){
-        if(minute<=20) return minute - 5;
-        else return (minute-20)/2+15;
+        if(minute<=25) return minute - 5;
+        else return (minute-25)/2+20;
     }
 }
